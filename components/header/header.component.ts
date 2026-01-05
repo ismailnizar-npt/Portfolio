e5036bb76b3f8a8bdc832145e461a0a715e4aa7d
Ismail N <ismailrasith86@gmail.com>
2026-01-05 18:32:15 +0400

    design changes

--- components/header/header.component.ts
+++ components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, Inject, PLATFORM_ID, inject } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RESUME } from '../../data/resume.data';
 
 @Component({
@@ -10,6 +10,44 @@ import { RESUME } from '../../data/resume.data';
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
     resume = RESUME;
+    activeSection: string = '';
+
+    private platformId = inject(PLATFORM_ID);
+    isBrowser = isPlatformBrowser(this.platformId);
+
+    constructor() { }
+
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        if (!this.isBrowser) return;
+
+        const sections = ['about', 'experience', 'skills', 'projects', 'contact'];
+
+        for (const section of sections) {
+            const element = document.getElementById(section);
+            if (element) {
+                const rect = element.getBoundingClientRect();
+                // If section is in the upper part of the screen
+                if (rect.top <= 150 && rect.bottom >= 150) {
+                    this.activeSection = section;
+                    break;
+                }
+            }
+        }
+    }
+
+    scrollToSection(sectionId: string, event: Event) {
+        event.preventDefault();
+        if (!this.isBrowser) return;
+
+        const element = document.getElementById(sectionId);
+        if (element) {
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+            this.activeSection = sectionId;
+        }
+    }
 }
